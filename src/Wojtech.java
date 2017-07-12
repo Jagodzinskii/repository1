6f6e9d13d2974652c2976c7ee8610cb16e7588dc
Jagodzinski <jagodzinski.lukasz83@gmail.com>
2017-07-12 20:30:47 +0200

    dodanie klasy Wojtech

---
+++ src/Wojtech.java
@@ -0,0 +1,18 @@
+/**
+ * Created by RENT on 2017-07-12.
+ */
+public class Wojtech {
+private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public Wojtech(int a) {
+        this.a = a;
+
+
+
+
+    }
+}
